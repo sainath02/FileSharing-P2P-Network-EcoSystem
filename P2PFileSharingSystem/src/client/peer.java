17f0b9a79f55f47f9d4382b191d0640937e3e2c8
Siva Sainath Tallapragada <Siva Sainath Tallapragada>
2016-07-19 17:12:01 -0400

    Complete logic for peer to peer sharing of files

--- P2PFileSharingSystem/src/client/peer.java
+++ P2PFileSharingSystem/src/client/peer.java
@@ -1,11 +1,15 @@
 package client;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,12 +17,13 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
 import java.util.logging.Handler;
 import java.util.logging.Logger;
 
-import server.Server;
+//import server.Server;
 
-public class peer {
+public class Peer {
 
 	private static int serverPort = 8080;
 	private static int clientPort = -1;
@@ -26,25 +31,40 @@ public class peer {
 	private static String hostName = "localhost";
 	private static ArrayList<Integer> chunksList = null;
 	private static HashMap<String, Long> chunks = new HashMap<>();
-	private static int chunkscount;
-	private static String peerDir = "PeerDB" + serverPort;
+	private static int chunkscount = 10;
+	private static int noOfPeers;
+	private static int peerId;
+	private static String peerDir;
 	private static String serverFile = "serverFile.txt";
-	static final Logger log = Logger.getLogger(peer.class.getName());
+	private static int fileSize = 1000 * 1024;// Default size is 1000KB
+	public static final int chunkSize = 100 * 1024; // Each chunk having 100KB
+	public static String infoFile = "InformationFile";
+
+	static final Logger log = Logger.getLogger(Peer.class.getName());
 
 	public static void main(String[] args) {
 		// String serverName = args[0];
-		// serverPort = Integer.parseInt(args[0]);
-		// clientPort = Integer.parseInt(args[1]);
-		// neighborPort = Integer.parseInt(args[2]);
-		serverPort = 8080;
-		clientPort = 8000;
-		neighborPort = 8080;
+		if(args.length != 3){
+			System.out.println("Error in file input. Please provide server address, peer address, neighbour address in this order in batch file and rerun the program");
+			return;
+		}
+		serverPort = Integer.parseInt(args[0]);
+		clientPort = Integer.parseInt(args[1]);
+		neighborPort = Integer.parseInt(args[2]);
+//		serverPort = 8080;
+//		clientPort = 8000;
+//		neighborPort = 8000;
 
+		peerDir = "PeerDB" + clientPort;
+		
 		log.info("Peer: " + hostName + ":" + clientPort + " Server: " + hostName + ":" + serverPort + " Neighbor: "
 				+ hostName + ":" + neighborPort);
 
-		// getFileChunksFromServer();
+		getFileChunksFromServer();
+
 		new NeighbourConnect().start();
+
+		new ShareFile().start();
 
 	}
 
@@ -64,43 +84,39 @@ public class peer {
 			DataInputStream in = new DataInputStream(inFromServer);
 
 			out.writeUTF("Hello from " + client.getLocalSocketAddress());
-			System.out.println("Server says " + in.readInt());
+			out.flush();
+			// System.out.println("Server says " + in.readInt());
+			String peerInfoStr = (String) in.readUTF();
+			System.out.println("Server says " + peerInfoStr);
+			String[] peerInfo = peerInfoStr.split("#");
+			int peerId = Integer.parseInt(peerInfo[0]);
+			serverFile = peerInfo[1];
+			chunkscount = Integer.parseInt(peerInfo[2].trim());
+			noOfPeers = Integer.parseInt(peerInfo[3].trim());
 
-			out.writeUTF("Count of Chunks");
-			chunkscount = in.readInt();
 			chunksList = new ArrayList<>();
 			for (int i = 0; i < chunkscount; i++)
 				chunksList.add(i);
 
-			while (chunkscount > 0 && chunksList.size() >= 0) {
-				String next = null;
-				boolean flag = false;
-				if (flag) {
-					out.writeUTF("Get Next Chunk Name");
-					out.flush();
-					next = in.readUTF();
-					flag = false;
-				} else {
-					next = getNextChunk();
-					if (next == null) {
-						// System.out.println("Inside if next == null");
-						buildClientFile();
-						return;
-					}
-				}
-				System.out.println("Requesting file from server: " + next);
-				out.writeUTF("Get File" + "#" + next);
+			System.out.println(
+					"PeerId is " + peerId + " serverFile is " + serverFile + " chunks count is " + chunkscount);
 
-				System.out.println("Completed 2nd request");
+			int status = 0;
+			int nextChunk = peerId;
+			while (nextChunk < chunkscount) {
+				String chunkName = serverFile + "." + nextChunk;
+				out.writeUTF("Get File" + "#" + chunkName);
+				out.flush();
 
-				File f = new File(dir, next);
+				File f = new File(dir, chunkName);
 				f.createNewFile();
 				FileOutputStream fos = new FileOutputStream(f);
-				byte[] bytes = new byte[server.Server.chunkSize];
+				// Hard code chunkSize value
+				byte[] bytes = new byte[chunkSize];
 
 				int count;
 				int totalBytes = 0;
-				int size = server.Server.chunkSize;
+				int size = chunkSize;
 				while ((count = in.read(bytes)) >= 0) {
 					totalBytes += count;
 					System.out.println("Writing " + count + " total : " + totalBytes + " size : " + size);
@@ -111,14 +127,23 @@ public class peer {
 						break;
 				}
 				fos.flush();
-				System.out.println("File is created");
-			}
+				
+				updateChunksInfo(chunkName);
 
-			String chunkname = serverFile + ".1";
+				//chunks.put(chunkName, (long)chunkSize);
+				System.out.println("File is created");
+
+				status++;
+				nextChunk = peerId + status * noOfPeers;
+			}
+			if (chunks.size() == chunkscount) {
+				buildClientFile();
+			}
 
 			System.out.println("Closing the connection to server...");
 			out.writeUTF("close");
 			client.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -192,87 +217,134 @@ public class peer {
 				}
 			}
 
+			int neighborRequestAttempts = 0;
 			try {
 				OutputStream outToServer = neighbor.getOutputStream();
 				InputStream inFromServer = neighbor.getInputStream();
 
 				DataOutputStream out = new DataOutputStream(outToServer);
 				DataInputStream in = new DataInputStream(inFromServer);
+				ObjectInputStream inObj = new ObjectInputStream(inFromServer);
 
-				out.writeUTF("Hello from " + neighbor.getLocalSocketAddress());
-				System.out.println("Neighbor says " + in.readInt());
+				// chunkscount = 10;
+				System.out.println(chunkscount);
 
-				// Next four lines should be commented and handled in server
-				// connection thread.
-				out.writeUTF("Count of Chunks");
-				chunkscount = in.readInt();
-				chunksList = new ArrayList<>();
-				for (int i = 0; i < chunkscount; i++)
-					chunksList.add(i);
+				// chunksList = new ArrayList<>();
+				// for (int i = 0; i < chunkscount; i++)
+				// chunksList.add(i);
 
-				while (chunkscount > 0 && chunksList.size() >= 0) {
-					String next = null;
-					boolean flag = false;
-					next = getNextChunk();
-					if (next == null) {
+				while (true) {
+					out.writeUTF("Get Chunks Information");
+					HashMap<String, Long> neighborChunkList = (HashMap<String, Long>) inObj.readObject();
+
+					// refactor below names "diffList" , "getDiffOfSummary"
+					HashMap<String, Long> diffList = getDiffOfSummary(neighborChunkList);
+
+					if (diffList.size() == 0) {
+						if (neighborRequestAttempts >= 5) {
+							//getFileChunksFromServer();
+						} else {
+							neighborRequestAttempts++;
+							Thread.sleep(5000);
+							continue;
+						}
+					}
+
+					neighborRequestAttempts = 0;
+					log.info("Neighbour has " + diffList.size() + " different file shared");
+					for(String s : neighborChunkList.keySet()){
+						System.out.println(s);
+					}
+
+					for (String next : diffList.keySet()) {
+
+						System.out.println("Requesting file from Neighbour: " + next);
+						out.writeUTF("Get File" + "#" + next);
+						out.flush();
+
+						File f = new File(dir, next);
+						if (f.exists())
+							continue;
+
+						f.createNewFile();
+						FileOutputStream fos = new FileOutputStream(f);
+						byte[] bytes = new byte[chunkSize];
+
+						int count;
+						int totalBytes = 0;
+						int size = chunkSize;
+						// System.out.println("Recidue "+in.readUTF());
+						while ((count = in.read(bytes)) >= 0) {
+							totalBytes += count;
+							System.out.println("Writing " + count + " total : " + totalBytes + " size : " + size);
+							fos.write(bytes, 0, count);
+							System.out.println("Total Bytes: " + totalBytes);
+							System.out.println("chunkSize " + size);
+							if (totalBytes >= size) {
+								System.out.println("Copied complete file");
+								break;
+							}
+						}
+						fos.flush();
+						updateChunksInfo(next);
+						//chunks.put(next, (long) 100);
+						System.out.println("File is created");
+						log.info("Current residing files are");
+						for(String s : chunks.keySet()){
+							System.out.println(s);
+						}
+						
+					}
+					
+					if (chunks.size() == chunkscount) {
 						buildClientFile();
-						System.out.println("Closing the connection from neighbour...");
-						out.writeUTF("close");
-						return;
+						break;
 					}
-
-					System.out.println("Requesting file from server: " + next);
-					out.writeUTF("Get File" + "#" + next);
-
-					System.out.println("Completed 2nd request");
-
-					File f = new File(dir, next);
-					f.createNewFile();
-					FileOutputStream fos = new FileOutputStream(f);
-					byte[] bytes = new byte[server.Server.chunkSize];
-
-					int count;
-					int totalBytes = 0;
-					int size = server.Server.chunkSize;
-					while ((count = in.read(bytes)) >= 0) {
-						totalBytes += count;
-						System.out.println("Writing " + count + " total : " + totalBytes + " size : " + size);
-						fos.write(bytes, 0, count);
-						System.out.println("Total Bytes: " + totalBytes);
-						System.out.println("chunkSize " + size);
-						if (totalBytes == size)
-							break;
-					}
-					fos.flush();
-					System.out.println("File is created");
 				}
-
-				System.out.println("Closing the connection to neighbour...");
-				out.writeUTF("close");
-				neighbor.close();
+				
 			} catch (IOException e) {
 				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally{
+				if(neighbor != null && neighbor.isConnected()){
+					try {
+						neighbor.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 
-			// if (neighbor != null || !neighbor.isConnected()) {
-			// try {
-			// neighbor.close();
-			// } catch (IOException e) {
-			// e.printStackTrace();
-			// }
-			// }
+			if (chunks.size() == chunkscount) {
+				try {
+					buildClientFile();
+					log.info("File " + serverFile + " has be built in peerDB");
+					return;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
 		}
 	}
 
 	private static class ShareFile extends Thread {
-		
+
 		ServerSocket peerSocket = null;
 		DataInputStream in;
 		DataOutputStream out;
-		
+		ObjectOutputStream obj;
+
 		public void run() {
 			try {
-				peerSocket = new ServerSocket(serverPort);
+				peerSocket = new ServerSocket(clientPort);
 				System.out.println("Peer started sharing file");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -282,21 +354,70 @@ public class peer {
 			try {
 				while (true) {
 					Socket neighbor = peerSocket.accept();
-					System.out.println("Listening to peer "+ neighbor.getRemoteSocketAddress());
-					
+					System.out.println("Listening to peer " + neighbor.getRemoteSocketAddress());
+
 					in = new DataInputStream(neighbor.getInputStream());
-	    			out = new DataOutputStream(neighbor.getOutputStream());
-	    			
-	    			String request;
-	    			while(true){
-	    				try {
-	    					request = in.readUTF();
-	    					break;
-	    				}catch(Exception e){
-	    					//Exception ignored
-	    				}
-	    			}
-	    				    			
+					out = new DataOutputStream(neighbor.getOutputStream());
+					obj = new ObjectOutputStream(neighbor.getOutputStream());
+
+					String request;
+					while (true) {
+						try {
+							request = in.readUTF();
+							if (request.startsWith("Get Chunks Information")) {
+								log.info("Sending current Chunks Information to neighbour " + neighbor.toString());
+								obj.writeObject(summaryAsMap());
+//								HashMap<String, Long> tempMap = new HashMap<>();
+//								tempMap.put("serverFile.txt.3", 100L);
+//								tempMap.put("serverFile.txt.1", 100L);
+//								tempMap.put("serverFile.txt.2", 100L);
+//								tempMap.put("serverFile.txt.4", 100L);
+//								tempMap.put("serverFile.txt.6", 100L);
+//								tempMap.put("serverFile.txt.7", 100L);
+//								tempMap.put("serverFile.txt.8", 100L);
+//								//tempMap.put("serverFile.txt.9", 100L);
+//								obj.writeObject(tempMap);
+							} else if (request.startsWith("Get File")) {
+								System.out.println("Request Message is: " + request);
+								String filename = request.split("#")[1].trim();
+								log.info("Sending file with name " + filename);
+								//Change below dir name to peerDir
+								File f = new File("ServerDB", filename);
+
+								int count;
+								byte[] buffer = new byte[chunkSize];
+								OutputStream o = neighbor.getOutputStream();
+								// BufferedInputStream i = new
+								// BufferedInputStream(new FileInputStream(f));
+
+								FileInputStream i = new FileInputStream(f);
+
+								while ((count = i.read(buffer)) > 0) {
+									o.write(buffer, 0, count);
+									o.flush();
+								}
+								log.info("Sent file to neighbour");
+								// server.close();
+							} else if (request.startsWith("close")) {
+								log.info("Neighbour " + neighbor.getRemoteSocketAddress()
+										+ " has requested to close connection");
+								System.out.println("Closing connection");
+								neighbor.close();
+								break;
+							}
+							// These lines should be commented. You get file
+							// count from server.
+							else if (request.startsWith("Count of Chunks")) {
+								System.out.println("Request Message is: " + request);
+								out.writeInt(10);
+								out.flush();
+							}
+
+						} catch (Exception e) {
+							// Exception ignored
+						}
+					}
+
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -305,4 +426,37 @@ public class peer {
 
 		}
 	}
+	
+	private synchronized static void updateChunksInfo(String chunkName){
+		chunks.put(chunkName, (long)100);
+	}
+
+	// Refactor the name of SummaryAsMap
+//	private synchronized static HashMap<String, Long> summaryAsMap() {
+//		return chunks;
+//	}
+	private synchronized static HashMap<String, Long> summaryAsMap() {
+		
+		File dir = new File(peerDir);
+		if (!dir.exists()) return null;
+		
+		HashMap<String, Long> chunksInfo = new HashMap<>();
+		for(final File chunkEntry : dir.listFiles()){
+			chunksInfo.put(chunkEntry.getName(), 100L);
+		}
+
+		return chunksInfo;
+	}
+
+	private synchronized static HashMap<String, Long> getDiffOfSummary(HashMap<String, Long> neighborChunksList) {
+		HashMap<String, Long> out = (HashMap<String, Long>) neighborChunksList.clone();
+		if (neighborChunksList != null && neighborChunksList.size() > 0) {
+			HashMap<String, Long> primary = summaryAsMap();
+			for (String key : primary.keySet()) {
+				out.remove(key);
+			}
+		}
+		return out;
+	}
+
 }
--- P2PFileSharingSystem/src/server/Server.java
+++ P2PFileSharingSystem/src/server/Server.java
@@ -17,136 +17,138 @@ import java.util.Hashtable;
 import java.util.logging.Handler;
 import java.util.logging.Logger;
 
-
 public class Server {
 
 	static {
 		// Programmatic configuration
-		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] (%2$s) %5$s %6$s%n");
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+				"%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] (%2$s) %5$s %6$s%n");
 	}
 	static final Logger log = Logger.getLogger(Server.class.getName());
-	
-	private static int numOfChunks = 5;
+
+	private static int numOfChunks = 10;
+	private static int numOfPeers = 5;
 	public static String filename = "serverFile.txt";
 	public static String serverDir = "ServerDB";
 	public static String data = "I returned from the City about three o'clock on that \nMay afternoon pretty well disgusted with life. \nI had been three months in the Old Country, and was \nfed up with it. \nIf anyone had told me a year ago that I would have \nbeen feeling like that I should have laughed at him; \nbut there was the fact. \nThe weather made me liverish, \nthe talk of the ordinary Englishman made me sick, \nI couldn't get enough exercise, and the amusements \nof London seemed as flat as soda-water that \nhas been standing in the sun. \n'Richard Hannay,' I kept telling myself, 'you \nhave got into the wrong ditch, my friend, and \nyou had better climb out.";
-	public static final int chunkSize = 100 * 1024; //Each chunk having 100KB
+	public static final int chunkSize = 100 * 1024; // Each chunk having 100KB
 	private static Hashtable<String, Long> chunks = new Hashtable<>();
 	private static int serverPort = 8080;
-	
-	public static void main(String args[]) throws Exception{
+
+	public static void main(String args[]) throws Exception {
 		System.out.println("Hello Server. Please Upload ur file into Peer System");
-		
+
 		numOfChunks = Integer.parseInt(args[0]);
-		
+
 		createAndBreakFile(numOfChunks);
-		
-		//int serverPort = 8080;
+
+		// int serverPort = 8080;
 		ServerSocket sSocket = new ServerSocket(serverPort);
 		System.out.println("Started server socket");
-		
-		int clientNum = 1;
-    	try {
-        	while(true) {
-            		new ServeFile(sSocket.accept(),clientNum).start();
-            		System.out.println("Client "  + clientNum + " is connected!");
-			clientNum++;
-        			}
-    	} finally {
-        		sSocket.close();
-        		System.out.println("Distributed file is shared into peer to peer system");
-    	} 
-		
-		
-		//ServeFile sf = new ServeFile();
-		//Thread t = new Thread(sf);
-		//t.start();
-		
-//		try {
-//			System.out.println("Joining to main thread");
-//			t.join();
-//		} catch (InterruptedException e) {
-//			System.out.println("Main Thread has been interrupted");
-//			e.printStackTrace();
-//		}
-//		
-//		System.out.println("Distributed file can now be shared into peer to peer system");
-	}
-	
 
-	private static void createAndBreakFile(int numOfChunks){
+		int clientNum = 0;
+		try {
+			while (true) {
+				new ServeFile(sSocket.accept(), clientNum).start();
+				System.out.println("Client " + clientNum + " is connected!");
+				clientNum++;
+			}
+		} finally {
+			sSocket.close();
+			System.out.println("Distributed file is shared into peer to peer system");
+		}
+
+		// ServeFile sf = new ServeFile();
+		// Thread t = new Thread(sf);
+		// t.start();
+
+		// try {
+		// System.out.println("Joining to main thread");
+		// t.join();
+		// } catch (InterruptedException e) {
+		// System.out.println("Main Thread has been interrupted");
+		// e.printStackTrace();
+		// }
+		//
+		// System.out.println("Distributed file can now be shared into peer to
+		// peer system");
+	}
+
+	private static void createAndBreakFile(int numOfChunks) {
 		try {
 			File newFile = createFile(numOfChunks);
-			System.out.println("Parent: "+ newFile.getParent());
+			System.out.println("Parent: " + newFile.getParent());
 			breakFile(newFile, numOfChunks);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private static File createFile(int numOfChunks) throws IOException{
+
+	private static File createFile(int numOfChunks) throws IOException {
 		File dir = new File(serverDir);
 		dir.mkdir();
 		File f = new File(dir, filename);
 		f.createNewFile();
-		
-		//Write if condition here	
-		if(!f.exists() || f.length() == 0){
+
+		// Write if condition here
+		if (!f.exists() || f.length() == 0) {
 			FileOutputStream fos = new FileOutputStream(f);
 			byte[] dataBytes = (data + " \n" + String.format("%08d", 0)).getBytes();
-			long m = (numOfChunks * chunkSize)/dataBytes.length;
-			
-			for(long i = 0; i< m; i++){
+			long m = (numOfChunks * chunkSize) / dataBytes.length;
+
+			for (long i = 0; i < m; i++) {
 				String ctr = String.format("%08d", i);
-				dataBytes = (ctr + " "+data + "\n").getBytes();
+				dataBytes = (ctr + " " + data + "\n").getBytes();
 				fos.write(dataBytes, 0, dataBytes.length);
 			}
-			
-			fos.write(dataBytes, 0, (int)((numOfChunks * chunkSize)%dataBytes.length));
+
+			fos.write(dataBytes, 0, (int) ((numOfChunks * chunkSize) % dataBytes.length));
 			fos.flush();
 			fos.close();
-			log.info("Created Server file: " + f.getName() + " of size:  "+ f.length() + " bytes");
+			log.info("Created Server file: " + f.getName() + " of size:  " + f.length() + " bytes");
 		}
-		
-		//f.delete();
-		//dir.delete();
+
+		// f.delete();
+		// dir.delete();
 		return f;
 	}
-	
+
 	private static void breakFile(File newFile, int numOfChunks2) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		if(newFile.length() == 0){
+		if (newFile.length() == 0) {
 			System.err.println("File size is 0");
-			return ;
+			return;
 		}
-		
+
 		int chunkNum = 0;
 		byte[] chunkBytes = new byte[chunkSize];
 		try {
 			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(newFile));
 			int temp = 0;
-			while ((temp = bis.read(chunkBytes)) > 0){
-				File chunk = new File(newFile.getParent(), newFile.getName()+"."+(chunkNum++));
+			while ((temp = bis.read(chunkBytes)) > 0) {
+				File chunk = new File(newFile.getParent(), newFile.getName() + "." + (chunkNum++));
 				FileOutputStream fos = new FileOutputStream(chunk);
 				fos.write(chunkBytes, 0, temp);
 				chunks.put(chunk.getName(), chunk.length());
-				
-				log.info("Created chunk file: "+ chunk.getName() + " of size: "+ chunk.length());
+
+				log.info("Created chunk file: " + chunk.getName() + " of size: " + chunk.length());
 				fos.close();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
- 
-	private static class ServeFile extends Thread{
-		
+
+	private static class ServeFile extends Thread {
+
 		private Socket server;
 		private int peerId;
-		public ServeFile(Socket connection, int no){
+		private int status = 0;
+
+		public ServeFile(Socket connection, int no) {
 			System.out.println("Connecting to peer");
 			this.server = connection;
 			this.peerId = no;
@@ -160,7 +162,7 @@ public class Server {
 		        try{
 		        	while(true){
 		        		System.out.println("Listening to client");
-		    			System.out.println("Connected to "+ server.getRemoteSocketAddress());
+		    			//System.out.println("Connected to "+ server.getRemoteSocketAddress());
 		    			
 		    			in = new DataInputStream(server.getInputStream());
 		    			out = new DataOutputStream(server.getOutputStream());
@@ -177,11 +179,29 @@ public class Server {
 		    			
 		    			if(request.startsWith("Hello")){
 		    				System.out.println("Request Message is: "+request);
-		    				out.writeInt(chunks.size());
+		    				String response = peerId + "#"+ filename + "#" + chunks.size() + "#" + numOfPeers;
+		    				//out.writeInt(chunks.size());
+		    				out.writeUTF(response);
+		    				out.flush();
 		    			}
 		    			else if(request.startsWith("Count of Chunks")){
 		    				System.out.println("Request Message is: "+request);
 		    				out.writeInt(chunks.size());
+		    				out.flush();
+		    			}
+		    			//Complete below logic
+		    			else if(request.startsWith("Get next chunk name")){
+		    				System.out.println("Request Message is: "+request);
+		    				int nextFile = peerId+status * numOfPeers;
+		    				if(nextFile < numOfChunks){
+		    					out.writeUTF(filename+"."+nextFile);
+		    					out.flush();
+		    					status++;
+		    				}
+		    				else{
+		    					out.writeUTF("Collect from peers");
+		    					out.flush();
+		    				}
 		    			}
 //		    			else if(request.startsWith("Get Next Chunk Name")){
 //		    				System.out.println("Request Message is: "+request);
@@ -192,7 +212,7 @@ public class Server {
 		    				System.out.println("Request Message is: "+request);
 		    				String filename = request.split("#")[1].trim();
 		    				log.info("Sending file with name "+ filename);
-		    				File f = new File("ServerDB",filename);
+		    				File f = new File(serverDir,filename);
 		    				
 		    				int count;
 		    				byte[] buffer = new byte[chunkSize];
@@ -207,7 +227,7 @@ public class Server {
 		    				//server.close();
 		    			}
 		    			else if(request.startsWith("close")){
-		    				log.info("Peer with id is requested to close connection");
+		    				log.info("Peer "+ server.getRemoteSocketAddress() + " has requested to close connection");
 		    				System.out.println("Closing connection");
 		    				server.close();
 		    				break;
@@ -227,7 +247,6 @@ public class Server {
 				}		    
 			
 		}
-		
+
 	}
-	
 }
